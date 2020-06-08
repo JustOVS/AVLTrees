4fc6ebbbe56ebe6d9552e9109b936637b6e5520a
Oleg <olegshitikov@gmail.com>
2020-06-09 00:09:31 +0300

    add AVLTree

---
+++ AVLTrees/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AVLTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVLTree<int> oak = new AVLTree<int>();
+
+            oak.Add(7);
+            oak.Add(2);
+            oak.Add(6);
+            oak.Add(3);
+            oak.Add(10);
+            oak.Add(13);
+            oak.Add(16);
+
+            foreach (var item in oak)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(oak.Contains(5));
+
+            oak.Remove(3);
+
+            foreach (var item in oak)
+            {
+                Console.WriteLine(item);
+            }
+
+            oak.Clear();
+
+            foreach (var item in oak)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
